d5bd8fa57ea7b5d6e0a501c4b1285b53a59c10b9
Bartłomiej Krawczyk <01158771@pw.edu.pl>
2022-06-07 18:29:07 +0200

    Added support via api - retrofit client

---
+++ src/main/java/com/example/pokemons/InterfaceApi.java
@@ -0,0 +1,18 @@
+package com.example.pokemons;
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+import retrofit2.http.Path;
+
+/**
+ * Interface with defined functions,
+ * used by retrofit to generate methods,
+ * that fetch data from server
+ *
+ * @see retrofit2.Retrofit
+ */
+public interface InterfaceApi {
+
+    @GET("type/{type}")
+    Call<Pokemon> getPokemonTypeInformation(@Path("type") String pokemonType);
+}
--- src/main/java/com/example/pokemons/PokemonApp.java
+++ src/main/java/com/example/pokemons/PokemonApp.java
@@ -1,7 +1,71 @@
 package com.example.pokemons;
 
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import okhttp3.OkHttpClient;
+import okhttp3.logging.HttpLoggingInterceptor;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+import java.util.Scanner;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * Base class for pokemon application
+ *
+ * PokemonApp:
+ * setups network connection,
+ * starts listening input for pokemons
+ * will return damage multiplier
+ */
 public class PokemonApp {
+
+    private static final String BASE_URL = "https://pokeapi.co/api/v2/";
+    public static InterfaceApi interfaceApi;
+
+    private static void createNetworkSys() {
+        HttpLoggingInterceptor loggingInterceptor = new HttpLoggingInterceptor();
+        loggingInterceptor.setLevel(HttpLoggingInterceptor.Level.BODY);
+
+        OkHttpClient okHttpClient = new OkHttpClient.Builder()
+                .addInterceptor(loggingInterceptor)
+                .writeTimeout(30, TimeUnit.SECONDS)
+                .readTimeout(30, TimeUnit.SECONDS)
+                .connectTimeout(30, TimeUnit.SECONDS)
+                .build();
+
+
+        Gson gson = new GsonBuilder()
+                .setDateFormat("yyyy-MM-dd")
+                .serializeNulls()
+                .setLenient()
+                .create();
+
+
+        Retrofit retrofit = new Retrofit.Builder()
+                .addConverterFactory(GsonConverterFactory.create(gson))
+                .baseUrl(BASE_URL)
+                .client(okHttpClient)
+                .build();
+
+        interfaceApi = retrofit.create(InterfaceApi.class);
+    }
+
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        createNetworkSys();
+
+        Scanner scanner = new Scanner(System.in);
+
+        while (scanner.hasNext()) {
+            String input = scanner.nextLine();
+            if (input.equals("exit")) {
+                return;
+            }
+
+            System.out.println(input);
+
+            String[] pokemons = input.split(" ");
+        }
+
     }
 }
